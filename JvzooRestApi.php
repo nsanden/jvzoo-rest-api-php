0f9cb2599cd9b1f80b8736aa2b0ef51e460066ab
Nate Sanden <natesanden@gmail.com>
2017-08-05 10:22:56 -0700

    added a helper method to strip bad jvzoo text from output if there is an error

--- JvzooRestApi.php
+++ JvzooRestApi.php
@@ -106,5 +106,24 @@ class JvzooRestApi {
         }
         return $response;
     }
+    
+    public function strip_tags_content($text, $tags = '', $invert = FALSE) {
 
+        preg_match_all('/<(.+?)[\s]*\/?[\s]*>/si', trim($tags), $tags);
+        $tags = array_unique($tags[1]);
+
+        if(is_array($tags) AND count($tags) > 0) {
+            if($invert == FALSE) {
+                return preg_replace('@<(?!(?:'. implode('|', $tags) .')\b)(\w+)\b.*?>.*?</\1>@si', '', $text);
+            }
+            else {
+                return preg_replace('@<('. implode('|', $tags) .')\b.*?>.*?</\1>@si', '', $text);
+            }
+        }
+        elseif($invert == FALSE) {
+            return preg_replace('@<(\w+)\b.*?>.*?</\1>@si', '', $text);
+        }
+        return $text;
+    }
+    
 }
